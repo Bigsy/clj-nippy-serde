35bd2e11342ddd8a1a23b405573be30377d9ceb3
eversC <chris.every@ovoenergy.com>
2020-11-04 09:20:31 +0000

    Bump taoensso/nippy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bigsy/clj-nippy-serde "0.1.0"
+(defproject bigsy/clj-nippy-serde "0.1.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
